8a5d562f2ce36314cab583c2f67158b5eadf0f82
AriosaWanqara <jariosa@wanqara.com>
2023-09-28 16:58:23 -0500

    add dagger hilt in retrofit object

--- app/src/main/java/com/example/printermobile/di/RetrofitModule.kt
+++ app/src/main/java/com/example/printermobile/di/RetrofitModule.kt
@@ -1,11 +1,19 @@
 package com.example.printermobile.core
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitHelper {
-
-    fun getRetrofit():Retrofit{
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("")
             .addConverterFactory(GsonConverterFactory.create())
